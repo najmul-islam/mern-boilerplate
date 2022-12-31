d5298266cdb67a5c4cad6a4cd6f4175cf686dced
Najmul Islam <66243645+najmul-islam@users.noreply.github.com>
2022-12-31 17:53:09 +0000

    change server.js to app.js and some stuf

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,7 @@
-import * as React from "react";
+import React from "react"
 
-export default function BasicButtons() {
-  return <h1>Home</h1>;
+const Home = () => {
+  return <h1>Home </h1>
 }
+
+export default Home
